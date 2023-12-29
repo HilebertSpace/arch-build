48fd075623196ed75c9ba6ad90d83a587f940324
Community <61367732+HilebertSpace@users.noreply.github.com>
2023-12-29 09:27:48 +0800

    update shell script

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
+BASEDIR="$PWD"
 FILE="$(basename "$0")"
 pkgname=$1
+
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -47,6 +49,7 @@ function prepend () {
 }
 
 pacman -S --noconfirm --needed namcap
+
 # For reasons that I don't understand, sudo is not resetting '$PATH'
 # As a result, namcap finds program paths in /usr/sbin instead of /usr/bin
 # which makes namcap fail to identify the packages that provide the
@@ -56,3 +59,28 @@ pacman -S --noconfirm --needed namcap
 # Work around this issue by putting bin ahead of sbin in $PATH
 export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 namcap PKGBUILD | prepend "::warning file=$FILE,line=$LINENO::"
+
+# Get array of packages to be built
+mapfile -t PKGFILES < <( sudo -u builder makepkg --packagelist )
+echo "Package(s): ${PKGFILES[*]}"
+
+# Report built package archives
+i=0
+for PKGFILE in "${PKGFILES[@]}"; do
+    # makepkg reports absolute paths, must be relative for use by other actions
+    RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
+    # Caller arguments to makepkg may mean the pacakge is not built
+    if [ -f "$PKGFILE" ]; then
+        echo "pkgfile$i=$RELPKGFILE" >> $GITHUB_OUTPUT
+    else
+        echo "Archive $RELPKGFILE not built"
+    fi
+    (( ++i ))
+done
+
+for PKGFILE in "${PKGFILES[@]}"; do
+    if [ -f "$PKGFILE" ]; then
+        RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
+        namcap "$PKGFILE" | prepend "::warning file=$FILE,line=$LINENO::$RELPKGFILE:"
+    fi
+done
