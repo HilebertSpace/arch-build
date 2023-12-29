e5c00713282436d4da3fe74baa892595ee28ea09
Community <61367732+HilebertSpace@users.noreply.github.com>
2023-12-29 10:52:54 +0800

    revert commit

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-BASEDIR="$PWD"
-FILE="$(basename "$0")"
 pkgname=$1
-
 
 useradd builder -m
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
@@ -40,47 +37,3 @@ fi
 sudo --set-home -u builder PATH="/usr/bin/vendor_perl:$PATH" paru -S --noconfirm --clonedir=./ "$pkgname"
 cd "./$pkgname" || exit 1
 python3 ../build-aur-action/encode_name.py
-
-function prepend () {
-    # Prepend the argument to each input line
-    while read -r line; do
-        echo "$1$line"
-    done
-}
-
-pacman -S --noconfirm --needed namcap
-
-# For reasons that I don't understand, sudo is not resetting '$PATH'
-# As a result, namcap finds program paths in /usr/sbin instead of /usr/bin
-# which makes namcap fail to identify the packages that provide the
-# program and so it emits spurious warnings.
-# More details: https://bugs.archlinux.org/task/66430
-#
-# Work around this issue by putting bin ahead of sbin in $PATH
-export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
-namcap PKGBUILD | prepend "::warning file=$FILE,line=$LINENO::"
-
-# Get array of packages to be built
-mapfile -t PKGFILES < <( sudo -u builder makepkg --packagelist )
-echo "Package(s): ${PKGFILES[*]}"
-
-# Report built package archives
-i=0
-for PKGFILE in "${PKGFILES[@]}"; do
-    # makepkg reports absolute paths, must be relative for use by other actions
-    RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
-    # Caller arguments to makepkg may mean the pacakge is not built
-    if [ -f "$PKGFILE" ]; then
-        echo "pkgfile$i=$RELPKGFILE" >> $GITHUB_OUTPUT
-    else
-        echo "Archive $RELPKGFILE not built"
-    fi
-    (( ++i ))
-done
-
-for PKGFILE in "${PKGFILES[@]}"; do
-    if [ -f "$PKGFILE" ]; then
-        RELPKGFILE="$(realpath --relative-base="$BASEDIR" "$PKGFILE")"
-        namcap "$PKGFILE" | prepend "::warning file=$FILE,line=$LINENO::$RELPKGFILE:"
-    fi
-done
