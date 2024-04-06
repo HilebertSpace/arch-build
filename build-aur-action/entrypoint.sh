749d7b87f30f64d3a433c2068e4b2669f260901f
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-04-06 18:35:48 +0800

    Update entrypoint.sh
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -27,9 +27,9 @@ Server = https://mirrors.ocf.berkeley.edu/archlinuxcn/\$arch
 Server = https://archlinux.ccns.ncku.edu.tw/archlinuxcn/\$arch
 EOM
 
-pacman-key --init
 pacman-key --lsign-key "farseerfc@archlinux.org"
-pacman -Syu --noconfirm archlinuxcn-keyring paru
+pacman -Syu --noconfirm archlinuxcn-keyring
+pacman -Syu --noconfirm paru
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm ${INPUT_PREINSTALLPKGS}
 fi
