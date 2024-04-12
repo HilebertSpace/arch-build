b3895ae9fa2ffc7e20dc0fbe4e8f73b9d5f3f508
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-04-12 11:25:50 +0800

    add extra

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -19,10 +19,10 @@ Server = https://repo.archlinuxcn.org/\$arch
 EOM
 
 pacman-key --lsign-key "farseerfc@archlinux.org"
-pacman -Syu --noconfirm archlinuxcn-keyring
-pacman -Syu --noconfirm archlinuxcn-mirrorlist-git paru
+pacman -Syu --noconfirm archlinuxcn-keyring && pacman -Syu --noconfirm archlinuxcn-mirrorlist-git paru
 sed -i "s|^Server = https://repo.archlinuxcn.org/\$arch|Include = /etc/pacman.d/archlinuxcn-mirrorlist|g" /etc/pacman.conf
-cat /etc/pacman.conf
+pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 3056513887B78AEB
+pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm ${INPUT_PREINSTALLPKGS}
