638bdbb3158e650d916c474245f22ec93816790d
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-04-12 11:37:35 +0800

    add new repo

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -10,19 +10,18 @@ chmod -R a+rw .
 # Enable the cloudflare mirror
 sed -i '1i Server = https://cloudflaremirrors.com/archlinux/$repo/os/$arch' /etc/pacman.d/mirrorlist
 
-# Enable the multilib repository
+# Enable the multilib, archlinuxcn and chaotic aur repository
 cat << EOM >> /etc/pacman.conf
 [multilib]
 Include = /etc/pacman.d/mirrorlist
 [archlinuxcn]
 Server = https://repo.archlinuxcn.org/\$arch
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist
 EOM
 
-pacman-key --lsign-key "farseerfc@archlinux.org"
 pacman -Syu --noconfirm archlinuxcn-keyring && pacman -Syu --noconfirm archlinuxcn-mirrorlist-git paru
 sed -i "s|^Server = https://repo.archlinuxcn.org/\$arch|Include = /etc/pacman.d/archlinuxcn-mirrorlist|g" /etc/pacman.conf
-pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com && pacman-key --lsign-key 3056513887B78AEB
-pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 
 if [ ! -z "$INPUT_PREINSTALLPKGS" ]; then
     pacman -Syu --noconfirm ${INPUT_PREINSTALLPKGS}
