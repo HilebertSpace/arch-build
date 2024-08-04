100cf47aeb7421e2e043973f0cea82412bf71a8c
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-08-05 00:48:20 +0800

    Dev (#91)
    * commit
    
    * test
    
    * test
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update action.yml
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update build.yml
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    * Update entrypoint.sh
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -41,6 +41,7 @@ function set_path(){
 }
 
 set_path /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
-sudo -H -u builder env "PATH=${PATH}" paru -Syu --noconfirm --needed $([[ "$AUR_ONLY" == "true" ]] && echo --aur) --clonedir=./ "${pkgname}"
+echo "${IUPUT_AURONLY:+--aur}"
+sudo -H -u builder env "PATH=${PATH}" paru -Syu --noconfirm --needed $([[ "${INPUT_AURONLY}" == "true" ]] && echo --aur) --clonedir=./ "${pkgname}"
 cd "./${pkgname}" || exit 1
 python3 ../build-aur-action/encode_name.py
