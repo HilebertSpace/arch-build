3d4e0fd25f9e9db0b8c0da0bf52c6605b4fcb298
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-08-04 23:06:49 +0800

    Update entrypoint.sh
    Signed-off-by: Community <61367732+HilebertSpace@users.noreply.github.com>

--- build-aur-action/entrypoint.sh
+++ build-aur-action/entrypoint.sh
@@ -41,6 +41,6 @@ function set_path(){
 }
 
 set_path /usr/bin/site_perl /usr/bin/vendor_perl /usr/bin/core_perl
-sudo -H -u builder env "PATH=${PATH}" paru -Syu --noconfirm --needed $([[ "${INPUT_AUR_ONLY}" == "true" ]] && echo --aur) --clonedir=./ "${pkgname}"
+sudo -H -u builder env "PATH=${PATH}" paru -Syu --noconfirm --needed $([[ "${INPUT_AURONLY}" == "true" ]] && echo --aur) --clonedir=./ "${pkgname}"
 cd "./${pkgname}" || exit 1
 python3 ../build-aur-action/encode_name.py
