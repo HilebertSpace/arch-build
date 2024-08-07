e8b3e2d38acd4d69943327789bb50d8745b4e83f
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-08-07 15:42:04 +0800

    fix

--- build-nonaur-action/entrypoint.sh
+++ build-nonaur-action/entrypoint.sh
@@ -102,7 +102,7 @@ fi
 # Build packages
 # INPUT_MAKEPKGARGS is intentionally unquoted to allow arg splitting
 # shellcheck disable=SC2086
-sudo -H -u builder env "PATH=${PATH}" git config --global --add safe.directory "${PWD}"
+sudo -H -u builder env "PATH=${PATH}" git config --global --add safe.directory '*'
 sudo -H -u builder makepkg --syncdeps --noconfirm ${INPUT_MAKEPKGARGS:-}
 
 # Get array of packages to be built
