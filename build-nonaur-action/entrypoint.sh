94fd814c86c6873b66b55218278d88b8a5862cb1
Community <61367732+HilebertSpace@users.noreply.github.com>
2024-08-07 08:41:49 +0800

    test

--- build-nonaur-action/entrypoint.sh
+++ build-nonaur-action/entrypoint.sh
@@ -25,7 +25,9 @@ useradd builder -m
 # Give user `builder` passwordless sudo access
 echo "builder ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 
+ls -al /
 
+# Work around
 sudo -u builder env "PATH=${PATH}" git config --global safe.directory '*'
 
 # Give all users (particularly builder) full access to these files
